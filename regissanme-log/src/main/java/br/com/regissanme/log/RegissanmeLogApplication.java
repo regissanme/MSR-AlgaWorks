4246a0281963702709ee795804b9bb52853436d9
regissanme <rsanme@gmail.com>
2022-07-22 15:13:05 -0300

    Create main application

---
+++ regissanme-log/src/main/java/br/com/regissanme/log/RegissanmeLogApplication.java
@@ -0,0 +1,13 @@
+package br.com.regissanme.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegissanmeLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegissanmeLogApplication.class, args);
+	}
+
+}
