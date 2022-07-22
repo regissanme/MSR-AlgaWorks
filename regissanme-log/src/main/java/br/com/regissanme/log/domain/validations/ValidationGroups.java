2bd74e3267891779e8f32507afd7e402d21c23f0
regissanme <rsanme@gmail.com>
2022-07-22 20:17:25 -0300

    Create validation groups

---
+++ regissanme-log/src/main/java/br/com/regissanme/log/domain/validations/ValidationGroups.java
@@ -0,0 +1,12 @@
+package br.com.regissanme.log.domain.validations;
+
+/**
+ * Projeto: regissanme-log
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 22/07/2022
+ * Hora: 18:07
+ */
+public interface ValidationGroups {
+
+    interface ClienteId{}
+}
